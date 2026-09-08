4dc9b2bb1c640f5d70a2c0ee0b17324132d717eb
CleverCV <cleverpromx@gmail.com>
2026-09-07 20:23:55 -0600

    ola, ola  pasa,pasa

--- kernel/keyboard.cpp
+++ kernel/keyboard.cpp
@@ -14,6 +14,31 @@ static inline uint8_t inb(uint16_t port)
     return value;
 }
 
+volatile unsigned short* vga =
+    reinterpret_cast<volatile unsigned short*>(0xB8000);
+
+static int cursor = 0;
+
+static const char keyboard_map[128] =
+{
+    0,
+    27,
+    '1','2','3','4','5','6','7','8','9','0','-','=',
+    '\b',
+    '\t',
+    'q','w','e','r','t','y','u','i','o','p','[',']',
+    '\n',
+    0,
+    'a','s','d','f','g','h','j','k','l',';','\'','`',
+    0,
+    '\\',
+    'z','x','c','v','b','n','m',',','.','/',
+    0,
+    '*',
+    0,
+    ' ',
+};
+
 void keyboard_init()
 {
 }
@@ -25,4 +50,18 @@ extern "C" void keyboard_handler()
     // Ignorar key release
     if (scancode & 0x80)
         return;
+
+    if (scancode < 128)
+    {
+        char c = keyboard_map[scancode];
+
+        if (c != 0)
+        {
+            vga[cursor++] =
+                static_cast<unsigned short>(0x0F00 | c);
+
+            if (cursor >= 80)
+                cursor = 0;
+        }
+    }
 }
